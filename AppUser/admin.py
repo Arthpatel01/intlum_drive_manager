17f11c72a23d254a6399a19e1e49aa836f88f496
Arth Patel <arthpatel.ce@gmail.com>
2023-09-20 13:09:22 +0530

    Add by ARTH

---
+++ AppUser/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from AppUser.models import CustomUser
+
+# Register your models here.
+
+admin.site.register(CustomUser)
---
+++ AppUser/models.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+# Create your models here.
+
+# type_choices = (
+#     ('folder', 'folder'),
+#     ('File', 'File'),
+# )
+
+class CustomUser(AbstractUser):
+    phone_number = models.CharField(max_length=15, null=True, blank=True)
+    address = models.CharField(max_length=500, null=True, blank=True)
+    # type = models.CharField(choices=type_choices)
+
